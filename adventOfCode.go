df4a6da7772e78c076374f5eac6c431385ffb5bf
Herman Barnardt <herman.barnardt@standardbank.co.za>
2022-12-15 17:31:51 +0200

    feat: Added support to create/download for a year

--- adventOfCode.go
+++ adventOfCode.go
@@ -23,6 +23,7 @@ var allSolutions = make(map[int]map[int]solution)
 
 var filenameIncludesYear = false
 var fileExtension = ".go"
+var solutionDirectory = ""
 
 func Register(year int, dayNumber int, a, b func([]string) interface{}) {
 	if _, ok := allSolutions[year]; !ok {
@@ -46,12 +47,35 @@ func SetSolutionTemplate(s string) {
 	solutionTemplate = s
 }
 
+func SetSolutionDirectory(s string) {
+	solutionDirectory = s
+}
+
 func Run(cmd string, year int, day int, part int) error {
 	switch cmd {
 	case "create":
-		return create(year, day)
+		if day > 0 {
+			return create(year, day)
+		} else {
+			for i := 1; i <= 25; i++ {
+				if err := create(year, i); err != nil {
+					return err
+				}
+			}
+			return nil
+		}
 	case "download":
-		return download(year, day)
+		if day > 0 {
+			return download(year, day)
+		} else {
+			for i := 1; i <= 25; i++ {
+				if err := download(year, i); err != nil {
+					return err
+				}
+			}
+			return nil
+		}
+
 	case "solve":
 		return solve(fmt.Sprintf("./data/%d/day%d", year, day), year, day, part)
 	case "test":
@@ -62,17 +86,28 @@ func Run(cmd string, year int, day int, part int) error {
 	}
 }
 
-func makeDir(path string) {
+func makeDir(path string) (bool, error) {
 	if _, err := os.Stat(path); errors.Is(err, os.ErrNotExist) {
 		err := os.Mkdir(path, os.ModePerm)
 		if err != nil {
 			log.Println(err)
 		}
+		return true, nil
 	}
+	return false, nil
 }
 
-func getFile(year, day int) {
-	cookie, _ := readLines("session")
+func getFile(year, day int) error {
+	cookie, err := readLines("session")
+
+	if err != nil {
+		return err
+	}
+
+	if len(cookie) == 0 {
+		return errors.New("Missing session file")
+	}
+
 	url := fmt.Sprintf("https://adventofcode.com/%d/day/%d/input", year, day)
 	method := "GET"
 
@@ -80,34 +115,30 @@ func getFile(year, day int) {
 	req, err := http.NewRequest(method, url, nil)
 
 	if err != nil {
-		fmt.Println(err)
-		return
+		return err
 	}
 	req.AddCookie(&http.Cookie{Name: "session", Value: cookie[0]})
 
 	res, err := client.Do(req)
 	if err != nil {
-		fmt.Println(err)
-		return
+		return err
 	}
 	defer res.Body.Close()
 
 	body, err := ioutil.ReadAll(res.Body)
 	if err != nil {
-		fmt.Println(err)
-		return
+		return err
 	}
 
 	f, err := os.Create(fmt.Sprintf("./data/%d/day%d", year, day))
 	if err != nil {
-		fmt.Println(err)
-		return
+		return err
 	}
 	_, err = f.Write(body)
 	if err != nil {
-		fmt.Println(err)
-		return
+		return err
 	}
+	return nil
 }
 
 func readLines(file string) ([]string, error) {
@@ -136,44 +167,58 @@ func solve(path string, year, day, part int) error {
 	if _, ok := allSolutions[year][day]; !ok {
 		err := create(year, day)
 		if err != nil {
-			return fmt.Errorf("Solution not implemented\n%v", err)
+			return fmt.Errorf("Solution not implemented\nTemplate generation has failed %v", err)
 		}
 		return errors.New("Solution not implemented\nTemplate has been generated")
 	}
 
 	lines, err := readLines(path)
 
-	if err != nil || len(lines) == 0 {
-		return fmt.Errorf("Missing input file: %s", path)
+	if err != nil {
+		return fmt.Errorf("Error reading file: %s %v", path, err)
 	}
+	if len(lines) == 0 {
+		return fmt.Errorf("Input file %s is empty", path)
+	}
+
+	fmt.Printf("Solving %d Day %d\n", year, day)
 
 	if part == 1 || part == 0 {
 		startTime := time.Now()
 		solution1 := allSolutions[year][day].Part1(lines)
 		duration1 := time.Since(startTime)
-		fmt.Printf("Part 1\n%v (%v)\n", solution1, duration1)
+		fmt.Printf("Part 1: %v (%v)\n", solution1, duration1)
 	}
 	if part == 2 || part == 0 {
 		startTime := time.Now()
 		solution2 := allSolutions[year][day].Part2(lines)
 		duration2 := time.Since(startTime)
-		fmt.Printf("Part 2\n%v (%v)\n", solution2, duration2)
+		fmt.Printf("Part 2: %v (%v)\n", solution2, duration2)
 	}
 	return nil
 }
 
 func download(year, day int) error {
-	makeDir("./data")
-
 	path := fmt.Sprintf("./data/%d/day%d", year, day)
-	_, err := os.Open(path)
-	if errors.Is(err, os.ErrNotExist) {
-		fmt.Println("Downloading file")
-		makeDir(fmt.Sprintf("./data/%d", year))
-		getFile(year, day)
-	} else {
+	fmt.Println("Downloading file", path)
+	created, err := makeDir("./data")
+	if err != nil {
 		return err
 	}
+	if created {
+		fmt.Println("Created directory ./data")
+	}
+	created, err = makeDir(fmt.Sprintf("./data/%d", year))
+	if err != nil {
+		return err
+	}
+	if created {
+		fmt.Printf("Created directory ./data/%d\n", year)
+	}
+	if err = getFile(year, day); err != nil {
+		return err
+	}
+
 	return nil
 }
 
@@ -184,11 +229,13 @@ func create(year, day int) error {
 		filename = fmt.Sprintf("%dday%d", year, day)
 	}
 
+	fmt.Println("Creating file from template: ", filename)
+
 	basepath, err := os.Getwd()
 	if err != nil {
 		return err
 	}
-	filePath := path.Join(basepath, filename)
+	filePath := path.Join(basepath, solutionDirectory, filename)
 
 	_, err = os.Stat(filePath)
 	if !errors.Is(err, os.ErrNotExist) {
@@ -223,16 +270,13 @@ import (
 	aoc "github.com/herman-barnardt/aoc"
 )
 func init() {
-	aoc.Register(
-		{{.Year}},
-		{{.Day}},
-		solve{{.Year}}Day{{.Day}}Part1,
-		solve{{.Year}}Day{{.Day}}Part2,
-	)
+	aoc.Register({{.Year}}, {{.Day}}, solve{{.Year}}Day{{.Day}}Part1, solve{{.Year}}Day{{.Day}}Part2)
 }
+
 func solve{{.Year}}Day{{.Day}}Part1(lines []string) interface{} {
 	return errors.New("Not yet implemented")
 }
+
 func solve{{.Year}}Day{{.Day}}Part2(lines []string) interface{} {
 	return errors.New("Not yet implemented")
 }
