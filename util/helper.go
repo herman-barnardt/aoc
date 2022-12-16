468aefaec9471e85676b748ca487f3a6c6ff18a0
Herman Barnardt <herman.barnardt@standardbank.co.za>
2022-12-16 14:27:16 +0200

    feat: Add IntMin and IntMax functions

--- util/helper.go
+++ util/helper.go
@@ -14,6 +14,14 @@ func IntAbs(a int) int {
 	return int(math.Abs(float64(a)))
 }
 
+func IntMin(a, b int) int {
+	return int(math.Min(float64(a), float64(a)))
+}
+
+func IntMax(a, b int) int {
+	return int(math.Max(float64(a), float64(a)))
+}
+
 func IsType(v any, expectedType string) bool {
 	return fmt.Sprintf("%T", v) == expectedType
 }
