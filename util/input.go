a68ea130608624da07b0b7b4be947b24ecc34780
herman-barnardt <hmbarnardt@gmail.com>
2023-12-06 07:53:50 +0200

    Add split lines function

---
+++ util/input.go
@@ -0,0 +1,14 @@
+package util
+
+func SplitLines(lines []string, seperator string) [][]string {
+	retVal := make([][]string, 0)
+	temp := make([]string, 0)
+	for _, line := range lines {
+		if line == seperator {
+			retVal = append(retVal, temp)
+			temp = make([]string, 0)
+		}
+		temp = append(temp, line)
+	}
+	return retVal
+}
