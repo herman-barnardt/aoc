93a927269c824bd3b82db5c0cecfc2447603fb5e
herman-barnardt <hmbarnardt@gmail.com>
2023-12-06 11:32:17 +0200

    feat: Add Lines to maps functions

--- util/input.go
+++ util/input.go
@@ -1,5 +1,10 @@
 package util
 
+import (
+	"strconv"
+	"strings"
+)
+
 func SplitLines(lines []string, seperator string) [][]string {
 	retVal := make([][]string, 0)
 	temp := make([]string, 0)
@@ -12,5 +17,28 @@ func SplitLines(lines []string, seperator string) [][]string {
 		temp = append(temp, line)
 	}
 	retVal = append(retVal, temp)
+	return retVal
+}
+
+func LinesToMap(lines []string) map[int]map[int]string {
+	retVal := make(map[int]map[int]string)
+	for y, line := range lines {
+		retVal[y] = make(map[int]string)
+		for x, c := range strings.Split(line, "") {
+			retVal[y][x] = c
+		}
+	}
+	return retVal
+}
+
+func LinesToIntMap(lines []string) map[int]map[int]int {
+	retVal := make(map[int]map[int]int)
+	for y, line := range lines {
+		retVal[y] = make(map[int]int)
+		for x, c := range strings.Split(line, "") {
+			n, _ := strconv.Atoi(c)
+			retVal[y][x] = n
+		}
+	}
 	return retVal
 }
