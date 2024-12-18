1e874eb2eec78b3690bfb70e7f651f10fd1aa4ad
herman-barnardt <hmbarnardt@gmail.com>
2024-12-18 08:30:40 +0200

    feat: Add basic node implementation

--- graph/node.go
+++ graph/node.go
@@ -8,3 +8,31 @@ type Node interface {
 }
 
 type Path []Node
+
+type BasicNode struct {
+	X, Y               int
+	Value              string
+	PossibleNeighbours []*BasicNode
+	NeighbourFilter    func(*BasicNode) bool
+}
+
+func (p *BasicNode) GetNeighbours() []Node {
+	nodeNeighbours := make([]Node, 0)
+	for _, neighbour := range p.PossibleNeighbours {
+		if p.NeighbourFilter(neighbour) {
+			nodeNeighbours = append(nodeNeighbours, neighbour)
+		}
+	}
+	return nodeNeighbours
+}
+func (p *BasicNode) GetCost(to Node) float64 {
+	return 1
+}
+
+func (p *BasicNode) GetHeuristicCost(to Node) float64 {
+	return 1
+}
+
+func (p *BasicNode) Equal(to Node) bool {
+	return p.X == to.(*BasicNode).X && p.Y == to.(*BasicNode).Y
+}
